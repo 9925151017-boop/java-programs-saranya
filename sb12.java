66b4000b357385d733fa4f48f6041766a5f92a30
9925151017-boop <9925151017@klu.ac.in>
2026-07-18 12:19:12 +0530

    Add files via upload

---
+++ sb12.java
@@ -0,0 +1,11 @@
+public class sb12 {
+    public static void main(String[] args)
+    {
+        String s1="java python c";
+        String[] words=s1.split(" ");
+        for(String word : words){
+            System.out.println(word);
+        }
+    }
+    
+}
