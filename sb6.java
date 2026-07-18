d068f6204ea8b39ec2da7ec088f909bd512edd94
9925151017-boop <9925151017@klu.ac.in>
2026-07-18 12:15:38 +0530

    Add files via upload

---
+++ sb6.java
@@ -0,0 +1,9 @@
+public class sb6 {
+    public static void main(String[] args )
+    {
+        String s1="java";
+        String s2="javA";
+        System.out.println(s1.equalsIgnoreCase(s2));
+    }
+    
+}
