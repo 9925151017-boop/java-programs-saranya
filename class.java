f1379ce6ee4620dbc4fe0cbeb20942d2ce14f41b
9925151017-boop <9925151017@klu.ac.in>
2026-07-22 16:23:34 +0530

    Add files via upload

---
+++ class.java
@@ -0,0 +1,18 @@
+class student {
+    private int age;
+    public void setAge(int age){
+        if(age > 0){
+            this.age = age;
+        }
+    }
+    public int getAge(){
+        return age;
+    }
+    public class main {
+        public static void main(String[] args){
+            student s = new student();
+            s.setAge(20);
+            system.out.println(s.getAge());
+        }
+    }
+}
