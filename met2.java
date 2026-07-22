58465725d082ba73f7a15bd093539b8dd3bdb218
9925151017-boop <9925151017@klu.ac.in>
2026-07-22 16:26:08 +0530

    Add files via upload

---
+++ met2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class met2 {
+        static void checkBalance(double balance, double withdrawlAmount){
+            if(withdrawlAmount <= balace){
+                System.out.println("withdrawl successfully");
+                balance = balance - withdrawlAmount;
+                System.out.println("remaining balance: " +balance);
+            }else{
+                System.out.println("insufficiant balance");
+            }
+        }
+        public static void main(String[] args){
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Enter balance: ");
+            double balance = sc.nextDouble();
+            System.out.println("Enter withdrawl Amount: ");
+            double withdrawlAmount = sc.nextDouble();
+            checkBalance(balance,withdrawlAmount);
+        }
+    }
+    
+}
+
+    
+}
