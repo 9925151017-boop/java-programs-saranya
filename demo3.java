c99f2187403f2bdeee1aab6d9fb4642bd1d110e1
9925151017-boop <9925151017@klu.ac.in>
2026-07-22 16:23:58 +0530

    Add files via upload

---
+++ demo3.java
@@ -0,0 +1,12 @@
+public class demo3 {
+    static int getNumber(int a, int b)
+    {
+        return a+b;
+    }
+    public static void main(String[] args)
+    {
+        int result = getNumber(5,6);
+        System.out.println(result);
+    }
+    
+}
